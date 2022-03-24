395d96c7deca97a043cc995fd4774111d7a5c638
Thu Juli <thujuli037@gmail.com>
2022-03-24 10:20:46 +0800

    initial commit

---
+++ 10 Administration Model Comment/blogs/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Blog
+
+
+def index(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blogs/index.html', {'blogs': blogs})
