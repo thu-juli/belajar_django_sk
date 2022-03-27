b38f5b10c98dd661abb2423e95dd39200a422480
Thu Juli <thujuli037@gmail.com>
2022-03-28 06:39:30 +0800

    initial commit

---
+++ 15 Simple Validation/blogs/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from .models import Blog
+from django.http import HttpResponseRedirect
+
+
+def index(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blogs/index.html', {'blogs': blogs})
+
+
+def single(request, id):
+    blog = Blog.objects.get(pk=id)
+    return render(request, 'blogs/single.html', {'blog': blog})
+
+
+def comment(request, id):
+    blog = Blog.objects.get(pk=id)
+    if request.method == 'POST':
+        new_desc = request.POST['desc']
+        blog.comment_set.create(desc=new_desc)
+        return HttpResponseRedirect('/blogs')
+
+# def comment(request, id):
+#     blog = Blog.objects.get(pk=id)
+#     if request.method == 'POST':
+#         new_desc = request.POST['desc']
+#         blog.comment_set.create(desc=new_desc)
+#         return HttpResponseRedirect('/blogs')
