ba8d92cb33a8aa2610fc82fff0cf59e730187ed7
Thu Juli <thujuli037@gmail.com>
2022-04-06 07:35:44 +0800

    full CRUD

--- 19 Error Validation/blogs/urls.py
+++ 19 Error Validation/blogs/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blogs/<str:pk>/', views.blogs, name='blogs'),
     path('create-project/', views.createProject, name='create-project'),
+    path('update-project/<str:pk>/', views.updateProject, name='update-project'),
+    path('delete-project/<str:pk>/', views.deleteProject, name='delete-project'),
 ]
--- 19 Error Validation/blogs/views.py
+++ 19 Error Validation/blogs/views.py
@@ -22,7 +22,7 @@ def blogs(request, pk):
 
 
 def createProject(request):
-    form = ProjectForm
+    form = ProjectForm()
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid:
@@ -33,3 +33,33 @@ def createProject(request):
         'form': form,
     }
     return render(request, 'blogs/create-project.html', context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid:
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'blogs/create-project.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('index')
+
+    context = {
+        'project': project
+    }
+
+    return render(request, 'blogs/delete-project.html', context)
