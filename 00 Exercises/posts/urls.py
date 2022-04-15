7393229c94137a1dcc6ba3800c35e81489f20cd6
Thu Juli <thujuli037@gmail.com>
2022-04-15 20:07:04 +0800

    adding create post in modelform

--- 00 Exercises/posts/urls.py
+++ 00 Exercises/posts/urls.py
@@ -6,6 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name='posts'),
+    path('<str:pk>/', views.singlePost, name='single-post'),
+    path('create-post', views.createPost, name='create-post'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
