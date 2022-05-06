2c07aafa8ca44107a0b73b05ebcc6fc35cff7820
Thu Juli <thujuli037@gmail.com>
2022-05-07 04:45:45 +0800

    update queryset ListView

--- 00 Exercises/backend/urls.py
+++ 00 Exercises/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('blogs/<int:page>/', include('blogs.urls', namespace='article')),
+    path('blogs/<str:author>/<int:page>/',
+         include('blogs.urls', namespace='article')),
     path('admin/', admin.site.urls),
 ]
--- 00 Exercises/blogs/views.py
+++ 00 Exercises/blogs/views.py
@@ -13,11 +13,16 @@ class ArticleListView(ListView):
     ordering = ['title']
     paginate_by = 2
 
-    # def get_queryset(self, *args, **kwargs):
-    #     print(kwargs)
-    #     # self.queryset = Article.objects.filter()
-    #     return super().get_queryset()
+    def get_queryset(self, *args, **kwargs):
+        if self.kwargs['author'] != 'all':
+            self.queryset = Article.objects.filter(
+                author=self.kwargs['author'])
+            self.kwargs.update(
+                {'author': self.kwargs['author']}
+            )
+        return super().get_queryset()
 
     def get_context_data(self, **kwargs):
-        kwargs.update(self.extra_context)
+        self.kwargs.update(self.extra_context)
+        kwargs = self.kwargs
         return super().get_context_data(**kwargs)
