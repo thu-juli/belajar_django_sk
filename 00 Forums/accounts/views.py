5648fa194cc7de7de2840503d0ceb6f69ae8c181
Thu Juli <thujuli037@gmail.com>
2022-05-14 20:57:26 +0800

    add func to auto login after signup in model CBV

--- 00 Forums/accounts/views.py
+++ 00 Forums/accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, reverse
 from django.views.generic import CreateView
 from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -9,4 +11,16 @@ from .forms import SignUpForm
 class SignUpView(CreateView):
     template_name = 'registration/signup_form.html'
     form_class = SignUpForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        # save the new user first
+        form.save()
+        # get the username and password
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        # authenticate user then login
+        user = authenticate(
+            username=form.cleaned_data['username'], password=form.cleaned_data['password1'],)
+        login(self.request, user)
+        return redirect('home')
